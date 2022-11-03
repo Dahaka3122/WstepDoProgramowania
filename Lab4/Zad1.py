d6b35e2906f17cb613fba0007b0bc7f4998b4fd8
Dahaka3122 <KSwierczek312@outlook.com>
2022-11-03 15:05:35 +0100

    Wykonane kolejne zadania

---
+++ Lab4/Zad1.py
@@ -0,0 +1,13 @@
+"""Zadanie 1. Utwórz listę lista_zakupow zawierającą nazwy co najmniej czterech produktów spożywczych.
+Następnie:
+• wyświetl pierwszy i drugi element z listy
+• zmień trzeci element listy na „mleko”
+• wyświetl przedostatni i ostatni element listy"""
+
+x = ["chleb","jajka","szynka","ser"]
+print(x)
+print(x[0],x[1])
+x[2]="mleko"
+print(x[2])
+print(x[2],x[3])
+
